b5f57fec94dec94adf96a5da7f7516595abe1c98
composable-tu <208613607+composable-tu@users.noreply.github.com>
2026-04-16 11:23:39 +0800

    feat(简历管理): 添加上传简历功能
    在简历管理页面添加上传简历的抽屉组件，包含文件上传功能

--- src/pages/(main).vue
+++ src/pages/(main).vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
 import { RouterView, useRoute, useRouter } from "vue-router";
-import type { DropdownProps, HeadMenuProps } from "tdesign-vue-next";
+import { type DropdownProps, type HeadMenuProps, type UploadProps } from "tdesign-vue-next";
 import { ref } from "vue";
 import { useAccountStore } from "@/stores/account.ts";
+import { CloudUploadIcon } from "tdesign-icons-vue-next";
 
 const router = useRouter();
 const route = useRoute();
 const accountStore = useAccountStore();
+
+const uploadResumeVisible = ref(false);
 
 const menu1Value = ref(route.path === "/" ? "index" : route.path === "/resume" ? "resume" : "");
 const changeHandler: HeadMenuProps["onChange"] = (active) => {
@@ -31,6 +34,17 @@ const clickDropdownHandler: DropdownProps["onClick"] = (data) => {
     router.push("/login");
   }
 };
+
+const openUploadResumeDrawer = () => {
+  uploadResumeVisible.value = true;
+};
+
+const onClickUpload = () => {
+  console.log("upload");
+};
+
+const files = ref<UploadProps["value"]>([]);
+const uploadResumeRequest = 1; // TODO: 待完善
 </script>
 
 <template>
@@ -43,7 +57,13 @@ const clickDropdownHandler: DropdownProps["onClick"] = (data) => {
         <t-menu-item value="index">模拟面试</t-menu-item>
         <t-menu-item value="resume">简历管理</t-menu-item>
         <template #operations>
-          <t-dropdown :options="options" @click="clickDropdownHandler" trigger="click">
+          <t-button v-if="route.path === '/resume'" theme="primary" @click="openUploadResumeDrawer">
+            <template #icon>
+              <cloud-upload-icon />
+            </template>
+            上传简历
+          </t-button>
+          <t-dropdown :options="options" trigger="click" @click="clickDropdownHandler">
             <t-button shape="square" variant="text">
               <template #icon>
                 <t-icon name="user" />
@@ -57,6 +77,29 @@ const clickDropdownHandler: DropdownProps["onClick"] = (data) => {
       <RouterView />
     </t-content>
   </t-layout>
+  <t-drawer
+    v-model:visible="uploadResumeVisible"
+    :close-btn="true"
+    :on-confirm="onClickUpload"
+    confirmBtn="上传"
+    size="large"
+  >
+    <template #header>上传简历</template>
+    <t-upload
+      v-model="files"
+      :abridge-name="[10, 8]"
+      :allow-upload-duplicate-file="false"
+      :auto-upload="true"
+      :is-batch-upload="false"
+      :max="10"
+      :request-method="uploadResumeRequest"
+      :show-thumbnail="false"
+      :upload-all-files-in-one-request="false"
+      multiple
+      placeholder="支持批量上传文件，文件格式不限，最多只能上传 10 份文件"
+      theme="file-flow"
+    ></t-upload>
+  </t-drawer>
 </template>
 
 <style lang="less" scoped>
